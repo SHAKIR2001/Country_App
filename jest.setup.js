6dc4d7cb262c4074488ac66078292010fac3720c
shakir <shakircassim077@gmail.com>
2025-05-03 14:11:55 +0530

    testings

---
+++ jest.setup.js
@@ -0,0 +1,17 @@
+// filepath: c:\Users\SHAKIR\Desktop\Country React\jest.setup.js
+import '@testing-library/jest-dom';
+
+// Mock matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
